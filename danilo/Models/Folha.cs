fc069ba2bc7e11d0a2d9a1ad465759ebbf94e3da
danilo <danilomferreira96@gmail.com>
2024-05-02 10:22:37 -0300

    Models -> Funcionario/Folha

---
+++ danilo/Models/Folha.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace victor.Models;
+
+public class Folha
+{
+
+
+	public Folha()
+	{
+	}
+
+	public Folha(double valor, double quantidade, int mes, int ano, Funcionario funcionario)
+	{
+		Valor = valor;
+		Quantidade = quantidade;
+		Mes = mes;
+		Ano = ano;
+		Funcionario = funcionario;
+	}
+
+	
+	[Key]
+	public int Id { get; set; }
+	public double Valor { get; set; }
+	public double Quantidade { get; set; }
+	public int Mes { get; set; }
+	public int Ano { get; set; }
+
+	[ForeignKey("Funcionario")]
+	public int FuncionarioId { get; set; }
+	public Funcionario Funcionario { get; private set; }
+}
---
+++ danilo/Models/Funcionario.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace victor.Models;
+
+public class Funcionario
+{
+	public Funcionario()
+	{
+	}
+
+	public Funcionario(string nome, string cpf, int id, Folha folha)
+	{
+		Nome = nome;
+		Cpf = cpf;
+		Id = id;
+		Folha = folha;
+	}
+
+	[Key]
+	public int Id { get; set; }
+	public string Nome { get; set; }
+	public string Cpf { get; set; }
+
+
+	[ForeignKey("Folha")]
+	public int FolhaId { get; set; }
+	public Folha Folha { get; private set; }
+}
