6a6c1fe90169ea022e80917ae1b3d912c7ccbd26
danilo <danilomferreira96@gmail.com>
2024-05-02 11:24:03 -0300

    calculo folha

--- danilo/Models/Folha.cs
+++ danilo/Models/Folha.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace victor.Models;
 
@@ -20,7 +21,7 @@ public class Folha
 		Funcionario = funcionario;
 	}
 
-	
+
 	[Key]
 	public int Id { get; set; }
 	public double Valor { get; set; }
@@ -33,10 +34,43 @@ public class Folha
 	public Funcionario Funcionario { get; private set; }
 
 
-	
+	double bruto;
 
+	public double calcularBruto(double valor, double quantidade)
+	{
+		return bruto = valor * quantidade;
+	}
 
+	public double calcularIR(double bruto)
+	{
 
+		if (bruto > 1903.98)
+		{
+			return 142.80;
+		}
+		else if (bruto > 2826.66)
+		{
+			return 354.80;
+		}
+		else if (bruto > 3751.06)
+		{
+			return 636.13;
+		}
+		else if (bruto > 4664.68)
+		{
+			return 869.36;
+		}
+		return 0;
+	}
 
+	public double calcularFGTS(double bruto)
+	{
+		return bruto / (8 / 100);
+	}
 
+	public double calcularLiquido(double bruto)
+	{
+		double liquido = calcularBruto(Valor,Quantidade) - calcularFGTS(bruto) - calcularIR(bruto);
+		return liquido;
+	}
 }
